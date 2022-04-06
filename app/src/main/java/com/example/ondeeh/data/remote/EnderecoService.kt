5ae6b4bf0231fec3985be28674c1ffe7e8b5f30f
Matheus Gimenez <gimenez.lopes@hotmail.com>
2022-04-06 20:41:51 -0300

    Demonstracao do mvc

--- app/src/main/java/com/example/ondeeh/data/remote/EnderecoService.kt
+++ app/src/main/java/com/example/ondeeh/data/remote/EnderecoService.kt
@@ -1,5 +1,8 @@
 package com.example.ondeeh.data.remote
 import com.example.ondeeh.model.Endereco
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface EnderecoService {
     @GET("/ws/{cep}/json")
